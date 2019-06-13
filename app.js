b231d05a3f47d61ca7a1c0263d733822d485026c
ChineduOguejiofor <chineduknight@gmail.com>
2019-06-13 21:37:14 +0100

    made mooges online

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express'),
   flash = require("connect-flash"),
   methodOverride = require("method-override"),
   LocalStrategy = require("passport-local");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 //seedDB = require("./seed");
 
@@ -16,7 +16,11 @@ const commentRoutes = require("./routes/comments"),
   indexRoutes = require("./routes/index");
 
 //seedDB();//seed db ie remove all contents
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongodb://knight:<password>@yelpcamp-shard-00-00-pszhu.mongodb.net:27017,yelpcamp-shard-00-01-pszhu.mongodb.net:27017,yelpcamp-shard-00-02-pszhu.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority
+//mongoose.connect("mongodb://knight:yelppass@yelpcamp-pszhu.mongodb.net/test?retryWrites=true&w=majority");
+
+mongoose.connect("mongodb://knight:yelppass@yelpcamp-shard-00-00-pszhu.mongodb.net:27017,yelpcamp-shard-00-01-pszhu.mongodb.net:27017,yelpcamp-shard-00-02-pszhu.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
